3d4e71c8399a7b54c5f170ca1fb4e65c08eb6852
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 21:46:21 -0500

    feat(Crm): add manager status value object.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Model/ValueObjects/EManagerStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.ValueObjects;
+
+public enum EManagerStatus
+{
+    OPEN = 1,
+    CONTACTED = 2,
+    MEETING_SET = 3,
+    QUALIFIED = 4,
+    CUSTOMER = 5,
+    OPPORTUNITY_LOST = 6,
+    UNQUALIFIED = 7,
+    IN_VETERINARY_CUSTOMER = 8,
+}
