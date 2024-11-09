242ecf3f3491307d68fec6dbe0fe87888cd83004
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 22:51:08 -0500

    feat(Crm): add manager command service.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Services/IManagerCommandService.cs
@@ -0,0 +1,9 @@
+﻿using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
+using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Commands;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Services;
+
+public interface IManagerCommandService
+{
+    Task<Manager?> Handle(CreateManagerCommand command);
+}
