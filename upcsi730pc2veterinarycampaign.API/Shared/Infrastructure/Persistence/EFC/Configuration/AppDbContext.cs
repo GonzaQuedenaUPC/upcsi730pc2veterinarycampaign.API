e78e322ca8faa263b49006ac5468502eac419bdf
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-09 01:16:23 -0500

    feat: add db context mapping.

--- upcsi730pc2veterinarycampaign.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ upcsi730pc2veterinarycampaign.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
 using upcsi730pc2veterinarycampaign.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace upcsi730pc2veterinarycampaign.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -15,7 +16,18 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<Manager>().ToTable("Managers");
+        builder.Entity<Manager>().HasKey(m => m.Id);
+        builder.Entity<Manager>().Property(m => m.Id).IsRequired().ValueGeneratedOnAdd();
         
+        builder.Entity<Manager>().Property(m => m.FirstName).IsRequired().HasMaxLength(40);
+        builder.Entity<Manager>().Property(m => m.LastName).IsRequired().HasMaxLength(40);
+        builder.Entity<Manager>().Property(m => m.ApprovedAt).IsRequired();
+        builder.Entity<Manager>().Property(m => m.ReportedAt).IsRequired();
+        builder.Entity<Manager>().Property(m => m.ContactedAt).IsRequired();
+        builder.Entity<Manager>().Property(m => m.Status).IsRequired(); 
+
         builder.UseSnakeCaseNamingConvention();
     }
 }
