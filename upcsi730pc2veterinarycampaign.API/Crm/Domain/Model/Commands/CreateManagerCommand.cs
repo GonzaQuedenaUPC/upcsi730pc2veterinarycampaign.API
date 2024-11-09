6fa403e06e6a235cd69fcda6b2ff0193e34bcc7f
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 22:50:53 -0500

    feat(Crm): add create manager command.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Model/Commands/CreateManagerCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Commands;
+
+public record CreateManagerCommand(string FirstName, string LastName, 
+    int Status, int AssignedSalesAgentId, Guid VeterinaryCampaignManagerId);
