9a5a10ddef9e7dea0d6b7f469243ec5e7989d0d8
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 22:55:58 -0500

    feat(Crm): add manager repository implementation.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Infrastructure/Persistence/EFC/Repositories/ManagerRepository.cs
@@ -0,0 +1,15 @@
+﻿using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
+using upcsi730pc2veterinarycampaign.API.Crm.Domain.Repositories;
+using upcsi730pc2veterinarycampaign.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using upcsi730pc2veterinarycampaign.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Infrastructure.Persistence.EFC.Repositories;
+
+public class ManagerRepository(AppDbContext context)
+    : BaseRepository<Manager>(context), IManagerRepository
+{
+    public async Task<Manager?> FindByFirstNameAndLastNameAsync(string firstName, string lastName)
+    {
+        return Context.Set<Manager>().FirstOrDefault(m => m.FirstName == firstName && m.LastName == lastName);
+    }
+}
