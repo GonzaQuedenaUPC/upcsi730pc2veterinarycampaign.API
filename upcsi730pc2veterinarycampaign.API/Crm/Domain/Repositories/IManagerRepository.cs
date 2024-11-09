8ab3c1d24b33280fa4e4946b448ee60de5892c47
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 22:51:39 -0500

    feat(Crm): add manager repository.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Repositories/IManagerRepository.cs
@@ -0,0 +1,9 @@
+﻿using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
+using upcsi730pc2veterinarycampaign.API.Shared.Domain.Repositories;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Repositories;
+
+public interface IManagerRepository : IBaseRepository<Manager>
+{
+    Task<Manager?> FindByFirstNameAndLastNameAsync(string firstName, string lastName);
+}
