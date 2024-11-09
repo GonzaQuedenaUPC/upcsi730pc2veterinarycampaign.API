4726c9857e969422cccf31bd0a085361d0e5f671
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 22:08:42 -0500

    feat(Crm): add veterinary campaign manager id value object.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Model/ValueObjects/VeterinaryCampaignManagerId.cs
@@ -0,0 +1,22 @@
+﻿namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.ValueObjects;
+
+public record VeterinaryCampaignManagerId
+{
+    public string ManagerId { get; }
+
+    //Constructor sin parámetros que asigna un GUID por defecto como string
+    public VeterinaryCampaignManagerId() : this(Guid.NewGuid().ToString())
+    {
+    }
+
+    // Constructor principal con validación
+    public VeterinaryCampaignManagerId(string managerId)
+    {
+        if (string.IsNullOrWhiteSpace(managerId))
+        {
+            throw new ArgumentException("Manager ID cannot be null or empty", nameof(managerId));
+        }
+
+        ManagerId = managerId;
+    }
+}
