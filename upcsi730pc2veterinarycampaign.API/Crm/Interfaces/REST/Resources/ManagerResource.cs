8f7243d5bd4121196ede4fb2f332b0d8f2f56520
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 23:58:04 -0500

    feat(Crm): add manager resources.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Interfaces/REST/Resources/ManagerResource.cs
@@ -0,0 +1,4 @@
+﻿namespace upcsi730pc2veterinarycampaign.API.Crm.Interfaces.REST.Resources;
+
+public record ManagerResource(string FirstName, string LastName, int Status, DateTime ApprovedAt, DateTime ReportedAt,
+    DateTime ContactedAt, int AssignedSalesAgentId, Guid VeterinaryCampaignManagerId);
