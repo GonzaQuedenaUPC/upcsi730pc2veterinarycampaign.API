7e49186b5309336a25c4765160bda744d04220c0
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 22:42:05 -0500

    feat(Crm): add manager aggregate.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Model/Aggregates/Manager.cs
@@ -0,0 +1,51 @@
+﻿using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Commands;
+using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.ValueObjects;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
+
+public partial class Manager
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    
+    public int Status { get; private set; }
+    
+    public DateTime ApprovedAt { get; private set; }
+    public DateTime ReportedAt { get; private set; }
+    public DateTime ContactedAt { get; private set; }
+    
+    public AssignedSalesAgentId AgentId { get; private set; }
+    public VeterinaryCampaignManagerId VeterinaryId { get; private set; }
+    
+    public int AssignedSalesAgentId => AgentId.AgentId;
+    public Guid VeterinaryCampaignManagerId => VeterinaryId.Identifier;
+    
+    public Manager()
+    {
+        this.FirstName = string.Empty;
+        this.LastName = string.Empty;
+        this.Status = 0;
+        
+        this.ApprovedAt = new DateTime();
+        this.ReportedAt = new DateTime();
+        this.ContactedAt = new DateTime();
+        
+        this.AgentId = new AssignedSalesAgentId(0);
+        this.VeterinaryId = new VeterinaryCampaignManagerId();
+    }
+
+    public Manager(CreateManagerCommand command)
+    {
+        this.FirstName = command.FirstName;
+        this.LastName = command.LastName;
+        
+        this.Status = command.Status;
+
+        this.ApprovedAt = new DateTime();
+        this.ReportedAt = new DateTime();
+        this.ContactedAt = new DateTime();
+       
+        this.AgentId = new AssignedSalesAgentId(command.AssignedSalesAgentId);
+        this.VeterinaryId = new VeterinaryCampaignManagerId(command.VeterinaryCampaignManagerId);
+    }
+}
---
+++ upcsi730pc2veterinarycampaign.API/Crm/Domain/Model/Aggregates/ManagerAudit.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
+
+public partial class Manager
+{
+    //The CreatedDate and UpdatedDate properties are used to store the date and time when the entity was
+    //created and updated.
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
