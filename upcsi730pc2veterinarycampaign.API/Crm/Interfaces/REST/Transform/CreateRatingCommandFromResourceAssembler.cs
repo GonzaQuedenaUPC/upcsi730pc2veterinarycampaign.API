54b8e9bcca5ab4deea409051b73b21180f938455
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-08 23:58:16 -0500

    feat(Crm): add manager resources assemblers.

---
+++ upcsi730pc2veterinarycampaign.API/Crm/Interfaces/REST/Transform/CreateRatingCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+﻿using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Commands;
+using upcsi730pc2veterinarycampaign.API.Crm.Interfaces.REST.Resources;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Interfaces.REST.Transform;
+
+public class CreateRatingCommandFromResourceAssembler
+{
+    public static CreateManagerCommand ToCommandFromResource(CreateRatingResource resource)
+    {
+        return new CreateManagerCommand(resource.FirstName, resource.LastName, resource.Status, resource.ApprovedAt,
+            resource.ReportedAt, resource.ContactedAt, resource.AssignedSalesAgentId, new Guid());
+    }
+}
---
+++ upcsi730pc2veterinarycampaign.API/Crm/Interfaces/REST/Transform/ManagerResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using upcsi730pc2veterinarycampaign.API.Crm.Domain.Model.Aggregates;
+using upcsi730pc2veterinarycampaign.API.Crm.Interfaces.REST.Resources;
+
+namespace upcsi730pc2veterinarycampaign.API.Crm.Interfaces.REST.Transform;
+
+public class ManagerResourceFromEntityAssembler
+{
+    public static ManagerResource ToResourceFromEntity(Manager manager)
+    {
+        return new ManagerResource(manager.FirstName, manager.LastName, manager.Status, manager.ApprovedAt,
+            manager.ReportedAt, manager.ContactedAt, manager.AssignedSalesAgentId, manager.VeterinaryCampaignManagerId);
+    }
+}
